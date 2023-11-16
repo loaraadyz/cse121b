9c727c24f81553c128fe208a281049c3a4def5c6
loaraadyz <villal@byui.edu>
2023-11-16 17:06:13 +0800

    arranged folders

---
+++ sandbox/practice.js
@@ -0,0 +1,15 @@
+const points = [100,88,94,78,84,69];
+
+function convertPointsToStatus(pointTotal) {
+  let status = 'Not Complete';
+  if (pointTotal >= 93) {
+    status = 'Excellent';
+  } else if (pointTotal >= 70) {
+    status = 'Complete';
+  }
+  return status;
+}
+
+const statusReport = points.map(convertPointsToStatus);
+
+console.log(statusReport);
